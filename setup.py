85aabe9dcb42e6433c5b94d380eb1a9844868d02
Michael Kessler <mike@toadgrass.com>
2014-08-26 20:23:16 -0700

    Removed explicit PySide requirement for greater flexibility.
    Should revisit this in the future.

--- setup.py
+++ setup.py
@@ -229,7 +229,9 @@ setup(
 
     # List run-time dependencies here. These will be installed by pip when your
     # project is installed.
-    install_requires=['PySide'],
+    #install_requires=['PySide'],
+    #TODO: Make requirements PySide OR PyQt to allow for greater flexibility.
+    install_requires=[],
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
